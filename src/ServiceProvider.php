e0e206bfeeeb5a7557c5eafdc640e267b10d5a05
luoxiaojun <luoxiaojun@dodoca.com>
2018-10-10 16:32:57 +0800

    add vendor publish command

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,11 +2,34 @@
 
 namespace Lxj\Laravel\Tars;
 
+use Laravelista\LumenVendorPublish\VendorPublishCommand;
 use Lxj\Laravel\Tars\Commands\Deploy;
 use Lxj\Laravel\Tars\Commands\Tars;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerCommands();
+    }
+
+    /**
+     * Register commands.
+     */
+    protected function registerCommands()
+    {
+        $this->commands([
+            VendorPublishCommand::class,
+            Deploy::class,
+            Tars::class,
+        ]);
+    }
+
     /**
      * Perform post-registration booting of services.
      *
@@ -16,17 +39,6 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         $this->publishes([
             __DIR__ . '/index.php' => base_path('index.php'),
             __DIR__ . '/services.php' => base_path('services.php'),
-        ]);
-    }
-
-    /**
-     * Register commands.
-     */
-    protected function registerCommands()
-    {
-        $this->commands([
-            Deploy::class,
-            Tars::class,
         ]);
     }
 }
