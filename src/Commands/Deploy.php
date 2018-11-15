3ea1b997324f2f0249785cb4e7515ddfb2e7ed83
luoxiaojun <luoxiaojun@dodoca.com>
2018-11-15 18:19:51 +0800

    remove useless code

--- src/Commands/Deploy.php
+++ src/Commands/Deploy.php
@@ -10,11 +10,6 @@ class Deploy extends Command
 
     public function handle()
     {
-        $indexFileName = base_path('index.php');
-        if (!is_executable($indexFileName)) {
-            chmod($indexFileName, 0755);
-        }
-
         \Tars\deploy\Deploy::run();
     }
 }
