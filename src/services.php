5301a0b488b96c47ddd9533b493e7c36ad9855c3
luoxiaojun <luoxiaojun@dodoca.com>
2018-11-20 19:45:19 +0800

    bugfix

--- src/services.php
+++ src/services.php
@@ -1,4 +1,5 @@
 <?php
-// 以namespace的方式,在psr4的框架下对代码进行加载  
+// 以namespace的方式,在psr4的框架下对代码进行加载
+// 变量名这么长是为了防重复
 $laravelTarsTmpTarsConfig = include __DIR__ . '/config/tars.php';
 return $laravelTarsTmpTarsConfig['services'];
--- tars/tars.proto.php
+++ tars/tars.proto.php
@@ -1,3 +1,4 @@
 <?php
+// 变量名这么长是为了防重复
 $laravelTarsTmpTarsConfig = include __DIR__ . '/../src/config/tars.php';
 return $laravelTarsTmpTarsConfig['proto'];
