155c6a21196d3d17b792f1c7154e8faaa4290100
luoxiaojun <luoxiaojun@dodoca.com>
2018-11-20 17:43:14 +0800

    refactory: merge config

--- src/services.php
+++ src/services.php
@@ -1,17 +1,4 @@
-<?php  
+<?php
 // 以namespace的方式,在psr4的框架下对代码进行加载  
-return array(  
- 'namespaceName' => 'Lxj\Laravel\Tars\\',
- 'monitorStoreConf' => [
-   //'className' => Tars\monitor\cache\RedisStoreCache::class,
-   //'config' => [
-    // 'host' => '127.0.0.1',  
-    // 'port' => 6379,  
-    // 'password' => ':'
-   //],
-   'className' => Tars\monitor\cache\SwooleTableStoreCache::class,
-   'config' => [
-     'size' => 40960
-   ]
- ]
-);
+$tarsConfig = include __DIR__ . '/config/tars.php';
+return $tarsConfig['services'];
--- tars/tars.proto.php
+++ tars/tars.proto.php
@@ -1,12 +1,3 @@
 <?php
-return array(
-    'appName' => '{appName}', //根据实际情况替换
-    'serverName' => '{serverName}', //根据实际情况替换
-    'objName' => '{objName}', //根据实际情况替换
-    'withServant' => true, //决定是服务端,还是客户端的自动生成,true为服务端
-    'tarsFiles' => array(
-        //根据实际情况填写
-    ),
-    'dstPath' => '../src/app/Tars/servant', //可替换，遵循PSR-4规则
-    'namespacePrefix' => 'App\Tars\servant', //可替换，遵循PSR-4规则
-);
+$tarsConfig = include __DIR__ . '/../src/config/tars.php';
+return $tarsConfig['proto'];
