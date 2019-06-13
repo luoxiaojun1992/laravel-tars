12ecf6a074b5976dc71b0457a50ac15dfca8110f
luoxiaojun <luoxiaojun@dodoca.com>
2019-06-13 23:00:41 +0800

    service config refactor

--- src/services.php
+++ src/services.php
@@ -1,5 +1,3 @@
 <?php
 // 以namespace的方式,在psr4的框架下对代码进行加载
-// 变量名这么长是为了防重复
-$laravelTarsTmpTarsConfig = include __DIR__ . '/config/tars.php';
-return $laravelTarsTmpTarsConfig['services'];
+return (include __DIR__ . '/config/tars.php')['services'];
