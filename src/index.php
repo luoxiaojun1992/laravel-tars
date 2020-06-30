40aa75bf6c1984fe16808a2543626a86c04307f8
罗晓俊 <luoxiaojun1992@sina.cn>
2020-06-30 22:46:36 +0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,10 +1,10 @@
 <?php
 //php tarsCmd.php  conf restart
 
-$config_path = $argv[1];
+$config_path = $argv[1] ?? ($_SERVER['argv'][1]);
 $pos = strpos($config_path, '--config=');
 $config_path = substr($config_path, $pos + 9);
-$cmd = strtolower($argv[2]);
+$cmd = strtolower($argv[2] ?? ($_SERVER['argv'][2]));
 
 if ($cmd === 'stop') {
     include_once __DIR__ . '/vendor/autoload.php';
@@ -25,6 +25,7 @@ if ($cmd === 'stop') {
     $class = new \Tars\cmd\Command($cmd, $config_path);
     $class->run();
 } else {
+    $argv = $argv ?? [];
     $_SERVER['argv'][0] = $argv[0] = __DIR__ .'/artisan';
     $_SERVER['argv'][1] = $argv[1] = 'tars:entry';
     $_SERVER['argv'][2] = $argv[2] = '--cmd=' . $cmd;
